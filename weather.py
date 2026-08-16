3ff63326565c831fca222502f39735760653244c
dawn2028 <dawn2028@outlook.com>
2026-08-16 15:38:24 +0800

    增加weather 模块

---
+++ weather.py
@@ -0,0 +1,72 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+import datetime
+
+def get_weather(my_city):
+    urls = ["http://www.weather.com.cn/textFC/hb.shtml",
+            "http://www.weather.com.cn/textFC/db.shtml",
+            "http://www.weather.com.cn/textFC/hd.shtml",
+            "http://www.weather.com.cn/textFC/hz.shtml",
+            "http://www.weather.com.cn/textFC/hn.shtml",
+            "http://www.weather.com.cn/textFC/xb.shtml",
+            "http://www.weather.com.cn/textFC/xn.shtml"
+            ]
+    for url in urls:
+        resp = requests.get(url)
+        text = resp.content.decode("utf-8")
+        soup = BeautifulSoup(text, 'html5lib')
+        div_conMidtab = soup.find("div", class_="conMidtab")
+        tables = div_conMidtab.find_all("table")
+        for table in tables:
+            trs = table.find_all("tr")[2:]
+            for  tr in trs:
+                tds = tr.find_all("td")
+                # 这里倒着数，因为每个省会的td结构跟其他不一样
+                city_td = tds[-8]
+                this_city = list(city_td.stripped_strings)[0]
+                if this_city == my_city:
+
+                    high_temp_td = tds[-5]
+                    low_temp_td = tds[-2]
+                    weather_type_day_td = tds[-7]
+                    weather_type_night_td = tds[-4]
+                    wind_td_day = tds[-6]
+                    wind_td_day_night = tds[-3]
+
+                    high_temp = list(high_temp_td.stripped_strings)[0]
+                    low_temp = list(low_temp_td.stripped_strings)[0]
+                    weather_typ_day = list(weather_type_day_td.stripped_strings)[0]
+                    weather_type_night = list(weather_type_night_td.stripped_strings)[0]
+
+                    wind_day = list(wind_td_day.stripped_strings)[0] + list(wind_td_day.stripped_strings)[1]
+                    wind_night = list(wind_td_day_night.stripped_strings)[0] + list(wind_td_day_night.stripped_strings)[1]
+
+                    # 如果没有白天的数据就使用夜间的
+                    temp = f"{low_temp}——{high_temp}摄氏度" if high_temp != "-" else f"{low_temp}摄氏度"
+                    weather_typ = weather_typ_day if weather_typ_day != "-" else weather_type_night
+                    wind = f"{wind_day}" if wind_day != "--" else f"{wind_night}"
+                    return this_city, weather_typ, temp, wind
+
+
+def get_daily_love():
+    url = "https://api.lovelive.tools/api/SweetNothings/Serialization/Json"
+    r = requests.get(url)
+    all_dict = json.loads(r.text)
+    sentence = all_dict['returnObj'][0]
+    daily_love = sentence
+    return daily_love
+
+def weather_report(this_city):
+    weather = get_weather(this_city)
+    print(f"今天：{datetime.date.today()}\n"
+    f"地区：{weather[0]}\n"
+    f"天气：{weather[1]}\n"
+    f"气温：{weather[2]}\n"
+    f"风向：{weather[3]}\n"
+    f"每日情话：{get_daily_love()}\n"
+    f"更新时间：{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}")
+
+
+if __name__ == '__main__':
+    weather_report("襄阳")
