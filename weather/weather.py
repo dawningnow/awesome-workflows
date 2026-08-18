8a9c14b26b82d487d4df84571364f9feb73b3e5e
dawn2028 <dawn2028@outlook.com>
2026-08-18 17:58:10 +0800

    qq email test

--- weather/weather.py
+++ weather/weather.py
@@ -137,5 +137,5 @@ def weather_report(receiver, this_city):
 
 
 if __name__ == '__main__':
-    weather_report("TEST_EMAIL", "武汉")
-    weather_report("TEST_EMAIL", "长春")
+    weather_report("MYSELF_EMAIL", "武汉")
+    weather_report("MYSELF_EMAIL", "长春")
