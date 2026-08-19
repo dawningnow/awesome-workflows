63481af4d232d13606bf6c550f7418eced0b4f52
dawn2028 <dawn2028@outlook.com>
2026-08-19 19:33:09 +0800

    weather test

--- weather/weather.py
+++ weather/weather.py
@@ -173,4 +173,4 @@ if __name__ == '__main__':
     sweetnothings = get_daily_love()
     daily_image = get_daily_image()
     weather_report(sweetnothings, daily_image, "MYSELF_EMAIL", "武汉")
-    # weather_report(sweetnothings, daily_image,"CH_EMAIL", "长春")
+    weather_report(sweetnothings, daily_image,"MYSELF_EMAIL", "长春")
