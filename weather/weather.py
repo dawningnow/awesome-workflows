e5f9050862c8809f3912775ced79c9e9573a7cab
dawningnow <235528171+dawningnow@users.noreply.github.com>
2026-09-01 19:08:50 +0800

    update

--- weather/weather.py
+++ weather/weather.py
@@ -62,7 +62,7 @@ def get_daily_love():
     url = "https://v1.hitokoto.cn/"
     res = requests.get(url)
     data = res.json()
-    return data['hitokoto'] + '<br>--- ' + data['from']
+    return data['hitokoto'] + '     --- ' + data['from']
 
 
 def get_daily_image():
